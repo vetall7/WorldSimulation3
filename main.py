ee994f9d40359787ed99635ee4b2ec926bffc089
Vitalii Shapovalov <101347133+vetall7@users.noreply.github.com>
2023-06-09 22:01:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,81 @@
 import tkinter as tk
-from Main.World import World
+from Main.SimpleWorld import SimpleWorld
+from Main.HexWorld import HexWorld
 from Main.WorldGenerator import WorldGenerator
+from tkinter import messagebox
 
-def get_values():
-    width = int(width_entry.get())
-    height = int(height_entry.get())
-    world = World(height, width)
+def new_world():
+    window.destroy()
+    def create_world():
+        width = int(width_entry.get())
+        height = int(height_entry.get())
+        root.destroy()
+        world = HexWorld(height, width)
+        generate = WorldGenerator(world)
+        generate.Generate()
+        world.DrawWorld(generate)
+        root.destroy()
+
+    root = tk.Tk()
+    root.title("Диалоговое окно")
+    root.geometry("400x250")
+    root.configure(bg="darkblue")
+
+    frame = tk.Frame(root, bg="darkblue")
+    frame.pack(padx=20, pady=20)
+
+    width_label = tk.Label(frame, text="Width:", bg="darkblue", fg="white", font=("Arial", 15))
+    width_label.pack()
+
+    width_entry = tk.Entry(frame, font=("Arial", 16))
+    width_entry.pack()
+
+    height_label = tk.Label(frame, text="Height:", bg="darkblue", fg="white", font=("Arial", 15))
+    height_label.pack()
+
+    height_entry = tk.Entry(frame, font=("Arial", 16))
+    height_entry.pack()
+
+    button = tk.Button(frame, text="Generate World", font=("Arial", 12), bg = "yellow", command=create_world)
+    button.pack(pady=30)
+
+    root.eval('tk::PlaceWindow . center')
+    root.mainloop()
+
+
+def saved_world():
+    window.destroy()
+    world = SimpleWorld(0,0)
     generate = WorldGenerator(world)
-    generate.Generate()
-    world.DrawWorld()
-    root.destroy()
+    world = generate.ReadGame()
+    world.DrawWorld(generate)
 
+# Создание главного окна
+window = tk.Tk()
+window.title("Диалоговое окно")
+window.geometry("300x250")  # Размер окна
+window.configure(bg="darkblue")  # Цвет фона окна
 
-root = tk.Tk()
-root.title("Диалоговое окно")
-root.geometry("300x150")
+# Создание надписи с жирным шрифтом, измененным цветом текста и фона
+label = tk.Label(window, text="WORLD GAME SIMULATION", font=("Helvetica", 14, "bold"), fg="white", bg="darkblue")
+label.pack(pady=10)  # Размещение надписи с отступом
 
-width_label = tk.Label(root, text="Ширина:")
-width_label.pack()
+# Создание контейнера для кнопок
+button_frame = tk.Frame(window, bg="darkblue")
+button_frame.pack(pady=10)  # Отступ между контейнером и надписью
 
-width_entry = tk.Entry(root)
-width_entry.pack()
+# Создание кнопок с овальной формой, увеличенными размерами и отступом
+button1 = tk.Button(button_frame, text="NEW GAME", command=new_world, bg="blue", fg="white", font=("Arial", 12, "bold"), padx=20, pady=10)
+button2 = tk.Button(button_frame, text="LOAD GAME", command=saved_world, bg="green", fg="white", font=("Arial", 12, "bold"), padx=20, pady=10)
 
-height_label = tk.Label(root, text="Высота:")
-height_label.pack()
+# Применение овальной формы кнопок
+button1.config(relief=tk.SOLID, bd=0)  # Убираем границу кнопки
+button1.pack(pady=10)  # Отступ между кнопками
+button2.config(relief=tk.SOLID, bd=0)
+button2.pack(pady=10)
 
-height_entry = tk.Entry(root)
-height_entry.pack()
+# Центрирование окна на экране
+window.eval('tk::PlaceWindow . center')
 
-button = tk.Button(root, text="Получить значения", command=get_values)
-button.pack()
-
-root.mainloop()
-
+# Запуск главного цикла обработки событий
+window.mainloop()
